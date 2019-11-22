8d4af8b1ca304589451a882b8f49f0619d75d479
Shannon Ervin <sjervin07@gmail.com>
2019-11-21 23:46:31 -0600

    adding text area to description

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-//Sets current date in Jumbotron//
+//Sets current date in Jumbotron
 
-let date = moment().format("dddd, MMMM Do")
-console.log('date', date)
+let date = moment().format("dddd, MMMM Do");
+console.log("date", date);
 
-let element = document.getElementById('currentDay')
-console.log(element)    
+let element = document.getElementById("currentDay");
+console.log(element);
 
-element.innerHTML = date
-//
+element.innerHTML = date;
 
-let currentTime = moment();
-console.log(currentTime)
+//Adds in color coding for past/current/future time
 
-
-
-let description = document.getElementsByClassName('description')
-
-//Adding in past/current/future time color coding//
-
-$("div.row").each(function(index, element){
-    console.log(element);
-let hour=element.getAttribute("data-hour");
-let currentHour = moment().format("HH")
-    if(hour===currentHour) {
-        element.classList.add("present")
-
-    }
-    else if (currentHour > hour)
-    {
-        element.classList.add("past")
-    }
-    else if (currentHour < hour)
-    {
-        console.log(currentHour)
-        console.log(hour)
-        element.classList.add ("future")
-    }
-    
+$("div.row").each(function(index, element) {
+  console.log(element);
+  let hour = element.getAttribute("data-hour");
+  let currentHour = moment().format("HH");
+  if (hour === currentHour) {
+    element.classList.add("present");
+  } else if (currentHour > hour) {
+    element.classList.add("past");
+  } else if (currentHour < hour) {
+    console.log(currentHour);
+    console.log(hour);
+    element.classList.add("future");
+  }
 });
-
-
-
